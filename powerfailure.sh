caa43f38bc56a49a2ecad6faacb1e0a391083eb4
Stuart Stephen <stuart.stephen@gmail.com>
2019-12-24 21:49:14 +0000

    Fixes issue on reboot as power last value is lost

--- powerfailure.sh
+++ powerfailure.sh
@@ -1,36 +1,42 @@
 #!/bin/bash
-powercsv=~/powerfailure.csv
-powerlast=/tmp/powerlast.tmp
+mkdir -p ~/powerfailure
+powercsv=~/powerfailure/powerfailure.csv
+powerlast=~/powerfailure/powerlast.tmp
 frequency=60                  # seconds
 margin=$(($frequency+($frequency/2)))
 if [ ! -f "$powercsv" ]; then
   echo "timestamp,weekofyear,dayofyear,day,date,month,year,hour,minute" > "$powercsv"
 fi
 if [ ! -f "$powerlast" ]; then
-  echo "4133980799" > "$powerlast"
+  echo "2000000000" > "$powerlast"
 fi
 last=$(($(cat "$powerlast"))) # seconds since epoch
 now=$(($(date +%s)))          # seconds since epoch
-lastdate=$(date -d @$last)
-nowdate=$(date -d @$now)
+lastdate=$(date -d "@$last")
+nowdate=$(date -d "@$now")
+# echo "Last: $last, Now: $now, Last Date: $lastdate, Now Date: $nowdate"
 if [ $now -lt $last ]; then
   # first run ever
   echo "$now" > "$powerlast"
+  echo "Reset power last variable"
 else
   # aim is to detect gaps greater than the run frequency.
   # echo "margin $margin frequency $frequency"
-  echo "$now" > "$powerlast"
+  # echo "$now" > "$powerlast"
   gap=$(($now-$last))
   if [ $gap -gt $margin ]; then
+    # echo Reported: $(date)
+    # echo Last: $last -> $(date -d "@$last")
+    # echo Current: $now -> $(date -d "@$now")
     echo "$nowdate: Power interruption detected at $lastdate! $gap exceeds $margin second limit."
-    weekofyear=$(date -d @$last +%V)
-    dayofyear=$(date -d @$last +%j)
-    day=$(date -d @$last +%a)
-    date=$(date -d @$last +%d)
-    month=$(date -d @$last +%m)
-    year=$(date -d @$last +%y)
-    hour=$(date -d @$last +%H)
-    minute=$(date -d @$last +%M)
+    weekofyear=$(date -d "@$last" +%V)
+    dayofyear=$(date -d "@$last" +%j)
+    day=$(date -d "@$last" +%a)
+    date=$(date -d "@$last" +%d)
+    month=$(date -d "@$last" +%m)
+    year=$(date -d "@$last" +%y)
+    hour=$(date -d "@$last" +%H)
+    minute=$(date -d "@$last" +%M)
     echo "$last,$weekofyear,$dayofyear,$day,$date,$month,$year,$hour,$minute" >> "$powercsv"
   fi
 fi
